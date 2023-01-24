235a3561ce7cb421a08196d3b96658cc143c9f53
celaltas <60509470+celaltas@users.noreply.github.com>
2023-01-25 02:08:49 +0300

    the project structured.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,17 +5,12 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from SnippetBox"))
-
-}
-
 func main() {
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-
 }
